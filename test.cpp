838d43804a293705948d7491c161818e31ce9344
wang-bin <wbsecg1@gmail.com>
2016-12-13 10:08:23 +0800

    update test

--- test.cpp
+++ test.cpp
@@ -3,15 +3,14 @@
  */
 #include "ThreadLocal.h"
 
-// default is to try c++11 thread_local: cxx ThreadLocal.cpp -DTEST_TLS c++11flags
-// mingw desktop use fiber api: g++ -DUSE_STD_THREAD_LOCAL=0 -std=c++11 ThreadLocal.cpp -D_WIN32_WINNT=0x0600  -DTEST_TLS
-// mingw desktop use pthread: g++ -DUSE_STD_THREAD_LOCAL=0 -std=c++11 ThreadLocal.cpp -DTEST_TLS
-// mingw store use fiber api: g++ -DUSE_STD_THREAD_LOCAL=0 -std=c++11 ThreadLocal.cpp  -DTEST_TLS
-// vc use fiber api: cl -DUSE_STD_THREAD_LOCAL=0 ThreadLocal.cpp /MD /EHsc  -DTEST_TLS
-// clang use pthread: clang++ -DUSE_STD_THREAD_LOCAL=0 ThreadLocal.cpp -DTEST_TLS
+// default is to try c++11 thread_local: cxx ThreadLocal.cpp c++11flags
+// mingw desktop use fiber api: g++ -DUSE_STD_THREAD_LOCAL=0 -std=c++11 ThreadLocal.cpp -D_WIN32_WINNT=0x0600 
+// mingw desktop use pthread: g++ -DUSE_STD_THREAD_LOCAL=0 -std=c++11 ThreadLocal.cpp
+// mingw store use fiber api: g++ -DUSE_STD_THREAD_LOCAL=0 -std=c++11 ThreadLocal.cpp 
+// vc use fiber api: cl -DUSE_STD_THREAD_LOCAL=0 ThreadLocal.cpp /MD /EHsc 
+// clang use pthread: clang++ -DUSE_STD_THREAD_LOCAL=0 ThreadLocal.cpp
 
-#ifdef TEST_TLS
-THREAD_LOCAL(int) a;
+THREAD_LOCAL(int) a = 11; // construct from T
 
 void setA(int* x, int v)
 {
@@ -21,14 +20,13 @@ void setA(int* x, int v)
 
 int main()
 {
-    a = 123;
+    std::cout << FUNCINFO << " initial a: " << (int)a << ", thread " << std::this_thread::get_id() << std::endl;
+    a = 123; // assignment
     std::thread t([]{
-        a = 456;
+        setA(&a, 456); // operator&()
         std::cout << FUNCINFO << " a: " << (int)a << ", thread " << std::this_thread::get_id() << std::endl;
     });
     t.join();
-    std::cout << FUNCINFO << " a: " << (int)a << ", thread " << std::this_thread::get_id() << std::endl;
-    setA(&a, 456);
     std::cout << FUNCINFO << " a: " << (int)a << ", thread " << std::this_thread::get_id() << std::endl;
     struct X {
         X(int v = 0) : x(v) {}
@@ -48,4 +46,3 @@ int main()
     std::cout << FUNCINFO << " x: " << X(x) << ", thread " << std::this_thread::get_id() << std::endl;
     return a;
 }
-#endif
