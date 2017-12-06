db56222989f0c88fdfd3d754509cfc105a625c48
wang-bin <wbsecg1@gmail.com>
2017-12-06 16:47:11 +0800

    update tests for move/copy

--- test.cpp
+++ test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 WangBin <wbsecg1 at gmail.com>/<binwang at pptv.com>
+ * Copyright (c) 2016-2017 WangBin <wbsecg1 at gmail.com>
  */
 #include "ThreadLocal.h"
 #include <iostream>
@@ -7,16 +7,35 @@
 #include <thread>
 using namespace std;
 // TODO: compare results and exit(result)
-// default is to try c++11 thread_local: cxx ThreadLocal.cpp c++11flags
-// mingw desktop use fiber api: g++ -DUSE_STD_THREAD_LOCAL=0 -std=c++11 ThreadLocal.cpp -D_WIN32_WINNT=0x0600 
-// mingw desktop use pthread: g++ -DUSE_STD_THREAD_LOCAL=0 -std=c++11 ThreadLocal.cpp
-// mingw store use fiber api: g++ -DUSE_STD_THREAD_LOCAL=0 -std=c++11 ThreadLocal.cpp 
-// vc use fiber api: cl -DUSE_STD_THREAD_LOCAL=0 ThreadLocal.cpp /MD /EHsc 
-// clang use pthread: clang++ -DUSE_STD_THREAD_LOCAL=0 ThreadLocal.cpp
+// default is to try c++11 thread_local: cxx test.cpp c++11flags
+// mingw desktop use fiber api: g++ -DUSE_STD_THREAD_LOCAL=0 -std=c++11 test.cpp -D_WIN32_WINNT=0x0600 
+// mingw desktop use pthread: g++ -DUSE_STD_THREAD_LOCAL=0 -std=c++11 test.cpp
+// mingw store use fiber api: g++ -DUSE_STD_THREAD_LOCAL=0 -std=c++11 test.cpp 
+// vc use fiber api: cl -DUSE_STD_THREAD_LOCAL=0 test.cpp /MD /EHsc 
+// clang use pthread: clang++ -DUSE_STD_THREAD_LOCAL=0 test.cpp
 
 struct X {
     X(const string& v = string()) : x(v) {
        std::cout << FUNCINFO <<this << ": " << x << ", thread " << this_thread::get_id() << std::endl;
+    }
+    X(string&& v) : x(std::move(v)) {
+        std::cout << FUNCINFO <<this << ": " << x << ", thread " << this_thread::get_id() << std::endl;
+    }
+    X(const X& y) : x(y.x) {
+        std::cout << FUNCINFO <<this << ": " << x << ", thread " << this_thread::get_id() << std::endl;        
+    }
+    X(X&& y) : x(std::move(y.x)) {
+        std::cout << FUNCINFO <<this << ": " << x << ", thread " << this_thread::get_id() << std::endl;
+    }
+    X& operator=(const X& y) {
+        x = y.x;
+        std::cout << FUNCINFO <<this << ": " << x << ", thread " << this_thread::get_id() << std::endl;        
+        return *this;
+    }
+    X& operator=(X&& y) {
+        x = std::move(y.x);
+        std::cout << FUNCINFO <<this << ": " << x << ", thread " << this_thread::get_id() << std::endl;        
+        return *this;
     }
     ~X() {
        std::cout << FUNCINFO <<this << ": " << x << ", thread " << this_thread::get_id() << std::endl;
@@ -30,18 +49,35 @@ void setA(int* x, int v)
     *x = v;
 }
 
-void test_assign()
+void test_assign_move()
 {
-    static THREAD_LOCAL(X) x = X("assing T");
+    std::cout << FUNCINFO << std::endl;
+    static THREAD_LOCAL(X) x = X("move assignment T");
 }
 
-void test_ctor()
+void test_assign_copy()
 {
-    static THREAD_LOCAL(X) x(X("ctor T"));
+    std::cout << FUNCINFO << std::endl;
+    X y = X("copy assignemnt T");
+    static THREAD_LOCAL(X) x = y;
+}
+
+void test_ctor_move()
+{
+    std::cout << FUNCINFO << std::endl;
+    static THREAD_LOCAL(X) x(X("move ctor T"));
+}
+
+void test_ctor_copy()
+{
+    std::cout << FUNCINFO << std::endl;
+    X y("copy ctor T");
+    static THREAD_LOCAL(X) x(y);
 }
 
 void test_ptr()
 {
+    std::cout << FUNCINFO << std::endl;
     static THREAD_LOCAL(X*) x(new X("T* delete test"));
     X* y = x;
     delete y;
@@ -49,17 +85,20 @@ void test_ptr()
 
 void test_operator_T()
 {
+    std::cout << FUNCINFO << std::endl;
     static THREAD_LOCAL(string) x(string("operator T"));
     std::cout << (string&)x << std::endl;
 }
 
 void test_operator_addressof()
 {
+    std::cout << FUNCINFO << std::endl;
     static THREAD_LOCAL(X) x(X("operator&() on THREADL_LOCAL like c++ thread_local test"));
     X *y = &x;
     *y = X("operator&() on THREAD_LOCAL like c++ thread_local test ok");
 }
 
+// TODO: array of ptr new T[N]
 int main()
 {
     thread(test_operator_addressof).join();
@@ -71,9 +110,9 @@ int main()
 
     thread(test_operator_T).join();
     thread(test_ptr).join();
-    thread(test_ctor).join();
-    thread(test_ctor).join();
-    thread(test_assign).join();
-    thread(test_assign).join();
+    thread(test_ctor_move).join();
+    thread(test_ctor_copy).join();
+    thread(test_assign_move).join();
+    thread(test_assign_copy).join();
     return 0;
 }
